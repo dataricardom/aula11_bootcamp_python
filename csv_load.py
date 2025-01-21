777e48b206ed3faa50cae628677c1b2827c0b8a6
Ricardo Marques <dataricardom@gmail.com>
2025-01-21 12:17:08 -0300

    Atualizando descrições

--- csv_load.py
+++ csv_load.py
@@ -1,3 +1,5 @@
+#Exemplo sem utilização de classe.
+
 import pandas as pd
 import time
 start = time.time()
@@ -6,8 +8,8 @@ path = 'data/exemplo.csv'
 
 df = pd.read_csv(path)
 
-# df_filtrado = df.query('estado == "SP"') #Execution = 0.009
-df_filtrado = df[df['estado'] == 'SP'] #Execution = 0.008
+# df_filtrado = df.query('estado == "SP"') #Execution time = 0.009
+df_filtrado = df[df['estado'] == 'SP'] #Execution time = 0.008
 
 print(df_filtrado)
 
